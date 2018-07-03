923af90e52a22e5b3f884248bc7871ebd5809adb
xdhuxc <xdhuxc@163.com>
2018-07-03 20:43:55 +0800

    etcd的使用

---
+++ app/util/etcd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests, json
+
+etcd_base_url = 'http://10.10.24.28:2379'
+
+
+def get_etcd_version():
+    current_uri = etcd_base_url + '/version'
+    resp = requests.get(current_uri)
+    # json.loads 用于解码JSON数据。该函数返回Python字段的数据类型。
+    version_json = resp.json()
+    return version_json['etcdserver']
+
+
+def get_all_keys():
+    current_uri = etcd_base_url + '/v2/keys'
+    resp = requests.get(current_uri)
+    return resp.json()
+
+
+def getKeyFromValue(key):
+    current_url = etcd_base_url + '/'
+
+if __name__ == '__main__':
+    get_all_keys()
